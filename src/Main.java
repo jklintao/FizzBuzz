8af0f6cb2e02d1d37503a09e06ebba8f5b73d3f7
kylelintao <jklintao@gmail.com>
2023-01-08 13:52:56 -0800

    fizzbuzz exercise with functions.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static int getUserInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number: ");
+        return scanner.nextInt();
+    }
+    public static void fizzbuzz(int a){
+        boolean fizz = a % 5 == 0;
+        boolean buzz = a % 3 == 0;
+
+        if (fizz && buzz){
+            System.out.println("FizzBuzz");
+        } else if (fizz) {
+            System.out.println("Fizz");
+        } else if (buzz) {
+            System.out.println("Buzz");
+        } else System.out.println(a);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        fizzbuzz(getUserInput());
     }
 }
